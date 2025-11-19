25b8fec2f0304698eb6b98a77f7c48de8008ca06
Eric Joh <yongminjoh@gmail.com>
2025-11-19 03:18:19 -0800

    port updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // preview server config
+  preview: {
+    port: 4174,        // 원하는 포트 번호
+    strictPort: true   // true면 해당 포트가 이미 사용 중일 경우 실행 실패
+  }
 })
